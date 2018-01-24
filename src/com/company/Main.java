01cf4c7621e7817375dd341ce599f42e4888b90d
jonathanredden <jonathancredden@gmail.com>
2018-01-24 16:37:32 -0500

    Learned And

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        int a = 2;
+        int b = 4;
+        int x = 6;
+        int y = 8;
 
+        System.out.println("a = 2");
+        System.out.println("b = 4");
+        System.out.println("c = 6");
+        System.out.println("d = 8");
 
-
-
-
+        if (a == x && y + y == a) {
+            System.out.println("a == x && y + y == a");
+            }
+        }
     }
-}
