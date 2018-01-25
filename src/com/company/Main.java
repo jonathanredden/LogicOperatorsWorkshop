bc98d3512d3dfd539513630db4f867f55e10d078
jonathanredden <jonathancredden@gmail.com>
2018-01-25 15:01:37 -0500

    Creating Projects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,29 @@ public class Main {
         int a = 2;
         int b = 4;
         int x = 6;
-        int y = 8;
+        int y = 1;
 
         System.out.println("a = 2");
         System.out.println("b = 4");
-        System.out.println("c = 6");
-        System.out.println("d = 8");
+        System.out.println("x = 6");
+        System.out.println("y = 1");
 
         if (a == x && y + y == a) {
             System.out.println("a == x && y + y == a");
+
+        }
+
+        //  && - AND - Both conditions have to be true to return true
+        //  || - OR - Only one condition has to be met to be true
+        //  ! - NOT -
+        //  == - Equal too - Validates condition
+
+        if (y + y == a || a == x) {
+            System.out.println("y + y == || a == x");
+            if(a < b){
+                System.out.println("a < b");
             }
         }
+
     }
+}
